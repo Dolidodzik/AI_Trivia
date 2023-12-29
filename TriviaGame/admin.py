b7368bdc4550ec2871fdac78dd5cfb900193ccc1
lesiu <lesiu@pop-os.localdomain>
2023-12-29 11:00:51 +0100

    created models

---
+++ TriviaGame/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import CountryPhoto, CountryFact
+
+admin.site.register(CountryPhoto)
+admin.site.register(CountryFact)
---
+++ TriviaGame/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class CountryInfoCommonData(models.Model):
+    country = models.CharField(max_length=100)
+    times_used_in_games = models.IntegerField(default=0) 
+    last_time_used_in_game = models.DateTimeField(auto_now_add=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class CountryPhoto(CountryInfoCommonData):
+    url = models.URLField(max_length=200)
+
+    # these are technically not necessary for now, but I am storing this data just in case
+    full_location_string = models.TextField()
+    full_URLs_list = models.TextField()
+
+class CountryFact(CountryInfoCommonData):
+    fact = models.URLField(max_length=200)
+
+
+
